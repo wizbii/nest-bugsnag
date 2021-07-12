097d32d957d2d9406e78917b685de26b6388bc24
Mickaël Depardon <mickael.depardon@wizbii.com>
2021-07-12 11:46:12 +0200

    Fix export constants

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './bugsnag.module';
 export * from './bugsnag.providers';
-export * from './bugsnag.providers';
+export * from './bugsnag.constants';
 export { BugsnagService } from './bugsnag.service';
 export { BugsnagLogger } from './bugsnag.logger';
 export { BugsnagClient } from './bugsnag.interfaces';
