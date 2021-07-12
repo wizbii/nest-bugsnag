f3334a2843e077820e37d5f7f44688796570ccf0
Mickaël Depardon <mickael.depardon@wizbii.com>
2021-07-12 11:06:32 +0200

    Export providers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './bugsnag.module';
+export * from './bugsnag.providers';
 export { BugsnagService } from './bugsnag.service';
 export { BugsnagLogger } from './bugsnag.logger';
 export { BugsnagClient } from './bugsnag.interfaces';
