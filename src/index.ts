22c58301d80388f805493db77135f32ab8c33d01
Mickaël Depardon <mickael.depardon@wizbii.com>
2021-07-12 11:26:17 +0200

    Export constants

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './bugsnag.module';
 export * from './bugsnag.providers';
+export * from './bugsnag.providers';
 export { BugsnagService } from './bugsnag.service';
 export { BugsnagLogger } from './bugsnag.logger';
 export { BugsnagClient } from './bugsnag.interfaces';
